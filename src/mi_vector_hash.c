d7d1a4bb17c371fb2d7d5edfb01306647027ba58
Mindaugas Rasiukevicius <rmind@netbsd.org>
2015-02-19 01:38:21 +0000

    Fix the #ifdef.

--- src/mi_vector_hash.c
+++ src/mi_vector_hash.c
@@ -48,6 +48,7 @@ __RCSID("$NetBSD: mi_vector_hash.c,v 1.1 2013/12/11 01:24:08 joerg Exp $");
 #if !HAVE_NBTOOL_CONFIG_H || HAVE_SYS_ENDIAN_H
 #ifdef __NetBSD__
 #include <sys/endian.h>
+#else
 #include <endian.h>
 #endif
 #endif
