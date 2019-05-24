5c8dc2f2fa0303543ddf810650afc6b2a5c4ad1b
Steve Wills <steve@mouf.net>
2019-05-24 04:31:51 -0400

    Enable build on FreeBSD

--- src/bitops.h
+++ src/bitops.h
@@ -36,12 +36,14 @@
 #else
 #include <stdint.h>
 
+#ifndef __GNUC_PREREQ__
 #ifdef __GNUC__
 #define	__GNUC_PREREQ__(x, y)						\
 	((__GNUC__ == (x) && __GNUC_MINOR__ >= (y)) ||			\
 	 (__GNUC__ > (x)))
 #else
 #define	__GNUC_PREREQ__(x, y)	0
+#endif
 #endif
 
 #ifndef __unused
--- src/cdbw.c
+++ src/cdbw.c
@@ -43,7 +43,7 @@ __RCSID("$NetBSD: cdbw.c,v 1.5 2012/07/21 22:49:37 joerg Exp $");
 #endif
 
 #if !HAVE_NBTOOL_CONFIG_H || HAVE_SYS_ENDIAN_H
-#ifdef __NetBSD__
+#if defined(__NetBSD__) || defined(__FreeBSD__)
 #include <sys/endian.h>
 #else
 #include <endian.h>
