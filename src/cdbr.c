a187cec5a4e32e9966db599028e8b590156ae4d6
Steve Wills <steve@mouf.net>
2019-05-24 17:59:14 -0400

    Enable build on FreeBSD (#1)

--- src/cdbr.c
+++ src/cdbr.c
@@ -52,7 +52,7 @@ __RCSID("$NetBSD: cdbr.c,v 1.1 2013/12/11 01:24:08 joerg Exp $");
 #endif
 #endif
 #if !HAVE_NBTOOL_CONFIG_H || HAVE_SYS_ENDIAN_H
-#ifdef __NetBSD__
+#if defined(__NetBSD__) || defined(__FreeBSD__)
 #include <sys/endian.h>
 #else
 #include <endian.h>
